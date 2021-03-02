dd49e6d19c5c92932f64dc5dc84fa333628961a2
Heather Storseth <heatherstorseth3@gmail.com>
2021-03-02 16:15:10 -0500

    5 day temp, icon, and humidity displays

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// help with api key
-// display things properlly to the webpage
 // local storage stuff last
 
 $(document).ready(function () {
@@ -19,12 +17,10 @@ $(document).ready(function () {
             .then(function (data) {
                 console.log(data);
                 let cityElement = $('<h2>').text(data.name);
-                let tempElement = $('<h2>').text(Math.round(data.main.temp) + String.fromCharCode(176));
+                let tempElement = $('<h2>').text('Temperature: ' + (Math.round(data.main.temp) + String.fromCharCode(176)));
                 // let dateElement = $('<h2>').text(data.timezone)
-                let humidElement = $('<h2>').text((data.main.humidity) + "%");
-                let windElement = $('<h2>').text(Math.round(data.wind.speed) + " mph");
-                
-                
+                let humidElement = $('<h2>').text('Humidity: ' + (data.main.humidity) + "%");
+                let windElement = $('<h2>').text('Wind Speed: ' + (Math.round(data.wind.speed) + " MPH"));
                 let icon = $('<img>').attr('src', 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png')
 
                 $('.outputContainer').attr('style', 'border: 2px solid black ; border-radius: 5px');
@@ -42,20 +38,19 @@ $(document).ready(function () {
 
 
                 get5day(data.coord.lat, data.coord.lon)
-                // }
             });
 
         console.log(requestUrl);
     }
 
     fetchBtn.addEventListener('click', function () {
-        var city = $('.input-box').val().trim()
+        let city = $('.input-box').val().trim()
         console.log(city);
         getApi(city)
     });
 
     function get5day(lat, lon) {
-        var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=adc7a83ef96c3c42fac6aa52f1b098b1&units=imperial`
+        let requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=adc7a83ef96c3c42fac6aa52f1b098b1&units=imperial`
 
         fetch(requestUrl)
             .then(function (response) {
@@ -64,32 +59,20 @@ $(document).ready(function () {
             })
             .then(function (data) {
                 console.log(data);
+
                 let uvElement = $('<h2>').text(Math.round(data.current.uvi) + ' uv');
-                $('.cityOutput').append(uvElement)
+                $('.cityOutput').append(uvElement);
+
+                for (let i = 0; i < 5; i++) {
+                    // let futureWeather = (data.daily[i]);
+                    
+                    let futureIcon = $('<img>').attr('src', 'http://openweathermap.org/img/w/' + data.daily[i].weather[0].icon + '.png');
+                    let futureTemp = $('<h3>').text('Temp: ' + (Math.round(data.daily[i].temp.day)) + String.fromCharCode(176));
+                    let futureHumid = $('<h3>').text('Humidity: ' + (data.daily[i].humidity) + '%')
+                    
+                    $('.weatherOutput').attr('style', 'border: 2px solid black ; border-radius: 5px');
+                    $('.daysForecast').append(futureTemp.append(futureIcon), futureHumid);
+                }
             })
     }
-
-
-
-
-
-    // let conditionElement = document.getElementById('condition');
-    // // let cityElement = document.getElementById('city');
-    // let dateElement = document.getElementById('date');
-    // let tempElement = document.getElementById('temp');
-    // let humidElement = document.getElementById('humid');
-    // let windElement = document.getElementById('wind');
-    // let uvElement = document.getElementById('uv');
-
-    // function currentWeatherData(data, place) {
-    //     conditionElement.textContent = data.
-    //     cityElement.textContent = place
-    //     dateElement.textContent = data.
-    //     tempElement.textContent = data.
-    //     humidElement.textContent = data.
-    //     windElement.textContent = data. 
-    //     uvElement.textContent = data.
-    // }
-
 })
-
