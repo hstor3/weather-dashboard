f4c46c088e4929245d997c3b5d608446df6d7b63
Heather Storseth <heatherstorseth3@gmail.com>
2021-03-06 17:18:40 -0500

    readme and final jungle sweep

---
+++ README.md
@@ -0,0 +1,24 @@
+# Weather Board
+
+## Overview
+
+HTML
+
+For this file, I created the elements that would be permanently displayed on the page. I then created sections for my fetched data/javaScript elements to be appended to. Lastly, I styled my webpage and used as much bootstrap as I could before editing the webpage in the CSS file.
+
+
+CSS
+
+For this file, I tried a gradient background, filled in the styling that I couldn't do with bootstrap, and created the five colors and sizes for the uv index box. 
+
+JavaScript
+
+For the JavaScript file, I mainly used jquery. I started by creating an onload function and from there, I started my fetching. I created a function with a URL, created elements that contained data from the URL and appended those elements to my desired sections. Next, I did the same thing for the 5 day forecast. Last, I made it so the searched cities saved to local storage.
+
+## Deployed Link
+
+
+
+## Screenshot 
+
+<img src='./weatherboard.png'>
--- script.js
+++ script.js
@@ -1,101 +1,92 @@
+// onload function
 $(document).ready(function () {
+    let day = moment().format('LL');
     searchHistory();
 
+    // list of cities that saves to local storage
     function searchHistory() {
         searches = JSON.parse(localStorage.getItem('searches') || '[]');
 
+        // loops through the amount of city lists
         for (let i = 0; i < searches.length; i++) {
-            //  = city[i];
 
-            let recent = $('<li class="city">').text(searches[i]);
+            let recent = $('<div class="city">').text(searches[i]);
 
             $('.citySearch').append(recent);
-
-            // get div to attatch city to
-            // append click funtion to div
-            // add to search box and make them clickable
         }
     }
 
+// city click function
     $(".city").on('click', function (event) {
-        console.log(event.target.outerText)    
-        // let city = event.text
-        console.log(event)
         getApi(event.target.outerText);
-        // get5day();
     })
 
-    // function cityClicks() {
-
-    // } 
-
-    // let cityOutput = document.getElementById('citySearch');
     let fetchBtn = document.getElementById('search-btn');
 
+// url to fetch info from
     function getApi(name) {
         let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + name + '&appid=adc7a83ef96c3c42fac6aa52f1b098b1&units=imperial'
 
         fetch(requestUrl)
             .then(function (response) {
                 return response.json();
-
             })
+// getting data from the url and creating elements
             .then(function (data) {
                 console.log(data);
                 let cityElement = $('<h2>').text(data.name);
-                let tempElement = $('<h2>').text('Temperature: ' + (Math.round(data.main.temp) + String.fromCharCode(176)));
-                // let dateElement = $('<h2>').text(data.timezone)
-                let humidElement = $('<h2>').text('Humidity: ' + (data.main.humidity) + "%");
-                let windElement = $('<h2>').text('Wind Speed: ' + (Math.round(data.wind.speed) + " MPH"));
+                let dateElement = $('<div>').text(day);
+                let tempElement = $('<div>').text('Temperature: ' + (Math.round(data.main.temp) + String.fromCharCode(176)));
+                let humidElement = $('<div>').text('Humidity: ' + (data.main.humidity) + "%");
+                let windElement = $('<div>').text('Wind Speed: ' + (Math.round(data.wind.speed) + " MPH"));
                 let icon = $('<img>').attr('src', 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png')
 
+// minimal styling to the output container and appending the above data to the city output container
                 $('.outputContainer').attr('style', 'border: 2px solid black ; border-radius: 5px');
                 $('.cityOutput').empty();
-                $('.cityOutput').append(cityElement.append(icon), tempElement, humidElement, windElement);
+                $('.cityOutput').append(cityElement.append(icon), dateElement, tempElement, humidElement, windElement);
 
                 get5day(data.coord.lat, data.coord.lon)
             });
-
-        console.log(requestUrl);
     }
 
+// button functionality/local storage stuff
     fetchBtn.addEventListener('click', function () {
         let city = $('.input-box').val().trim()
-        console.log(city);
         getApi(city);
 
         searches = JSON.parse(localStorage.getItem('searches') || '[]');
-        console.log(searches.includes(city))
         if (!searches.includes(city)) {
 
             searches.push(city)
 
-            let recent = $('<li>').text(city);
+            let recent = $('<div>').text(city);
             $('.citySearch').append(recent);
         }
         localStorage.setItem('searches', JSON.stringify(searches));
 
     });
 
-
+// 5 day forecast url to fetch from
     function get5day(lat, lon) {
         let requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=adc7a83ef96c3c42fac6aa52f1b098b1&units=imperial`
 
         fetch(requestUrl)
             .then(function (response) {
                 return response.json();
-
             })
+
             .then(function (data) {
                 console.log(data);
 
+// uv index data and where it's attatched in the HTML file
                 let uvNumb = Math.round(data.current.uvi)
-                let uvElement = $('<h2>').text(uvNumb + ' uv');
-                let square = $('<div>');
-                uvElement.append(square);
+                let square = $('<div class="m-auto">');
+                let uvElement = $('<div class="d-flex; text-center; pb-3">').text(uvNumb + ' uv index').append(square);
 
                 $('.cityOutput').append(uvElement);
 
+// if statement that changes the color of the uv index box
                 if (uvNumb <= 2 || uvNumb >= 0) {
                     square.addClass('green')
                 } else if (uvNumb <= 5 || uvNumb >= 3) {
@@ -107,30 +98,27 @@ $(document).ready(function () {
                 } else {
                     square.addClass('purple')
                 }
-                // else if (uvElement <=)
-                console.log(uvElement)
                 $('.weatherOutput').empty();
-
-                for (let i = 0; i < 5; i++) {
-                    // let futureWeather = (data.daily[i]);
-                    // let DateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(data.daily[i].dt);
-                    // let fiveDayDate = $('<h3>').text(data.daily[i].dt);
-                    // DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(fiveDayDate);
-                    // let fiveDayDate = new DateTime(1970, 1, 1, 0, 0, 0, 0, (data.daily[i].dt))); 
-                    // DateTimeKind.Utc);
-                    // DateTime time = startDate.AddSeconds(data.daily[i].{dt});
-
+                
+// loop to get the 5 days so i don't have to add each day individually
+                for (let i = 1; i < 6; i++) {
+// creating elements
                     let futureIcon = $('<img>').attr('src', 'http://openweathermap.org/img/w/' + data.daily[i].weather[0].icon + '.png');
-                    let futureTemp = $('<h3>').text('Temp: ' + (Math.round(data.daily[i].temp.day)) + String.fromCharCode(176));
-                    let futureHumid = $('<h3>').text('Humidity: ' + (data.daily[i].humidity) + '%');
+                    let futureTemp = $('<div class="temp">').text('Temp: ' + (Math.round(data.daily[i].temp.day)) + String.fromCharCode(176));
+                    let futureHumid = $('<div class="humidity">').text('Humidity: ' + (data.daily[i].humidity) + '%');
+                    
+                        var date = new Date(data.daily[i].dt * 1000);
+                        
+                        let futureDate = $('<span class="date">').text(date.toDateString());
 
-                    $('.outputContain').attr('style', 'border: 2px solid black ; border-radius: 5px');
-                    $('.weatherOutput').append(futureTemp.append(futureIcon), futureHumid);
-
-
+// minimal styling and where to attatch the created elements
+                    $('.weatherOutput').attr('style', 'border: 2px solid black ; border-radius: 5px ; bg-white');
+                    $('.weatherOutput').append(futureDate, futureTemp.append(futureIcon), futureHumid);
                 }
-                // savedSearches();
             })
     }
 
 })
+
+// look up how to autofill city search
+// add a keydown event handler
